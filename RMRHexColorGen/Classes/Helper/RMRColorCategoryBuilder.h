766e536858b08a9a591b8ee9555c07114c7d899d
Roman Churkin <rc@redmadrobot.com>
2015-03-25 16:30:57 +0300

    RMRConstants replaced with prefix header — RMRHexColorGenPrefixHeader.pch

--- RMRHexColorGen/Classes/Helper/RMRColorCategoryBuilder.h
+++ RMRHexColorGen/Classes/Helper/RMRColorCategoryBuilder.h
@@ -8,8 +8,6 @@
 
 @import Foundation;
 
-#import "RMRConstants.h"
-
 
 @interface RMRColorCategoryBuilder : NSObject
 
--- RMRHexColorGen/Classes/RMRConstants.m
+++
@@ -1,9 +0,0 @@
-//
-//  RMRConstants.m
-//  RMRHexColorGen
-//
-//  Created by Roman Churkin on 19/03/15.
-//  Copyright (c) 2015 RedMadRobot. All rights reserved.
-//
-
-#import "RMRConstants.h"
